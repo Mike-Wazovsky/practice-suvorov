013480b168e312e5cc6f510ff01e581fafada249
Mike-Wazovsky <71932958+Mike-Wazovsky@users.noreply.github.com>
2020-10-01 14:13:26 +0300

    Update 02-adjust.cpp

--- 04-201001/problem/02-adjust.cpp
+++ 04-201001/problem/02-adjust.cpp
@@ -6,7 +6,7 @@ using Container = std::list<int>;
 using ContainerIterator = Container::iterator;
 
 int main() {
-    Container data = {1, 2, 3, 4, 5};
+    Container data = { 1, 2, 3, 4, 5 };
 
     auto a = data.begin();
     auto b = a; ++b;
@@ -21,23 +21,23 @@ int main() {
     assert(*d == 4);
     assert(*e == 5);
 
-    assert(std::TODO(a) == b);
+    assert(std::next(a) == b);
     assert(*a == 1);
     assert(*b == 2);
 
-    assert(std::TODO(b) == a);
+    assert(std::prev(b) == a);
     assert(*a == 1);
     assert(*b == 2);
 
-    assert(std::TODO(a, TODO) == c);
+    assert(std::next(a, 2) == c);
     assert(*a == 1);
     assert(*c == 3);
 
-    assert(std::TODO(c, TODO) == a);
+    assert(std::prev(c, 2) == a);
     assert(*a == 1);
     assert(*c == 3);
 
-    std::TODO(a, 2);
+    std::advance(a, 2);
     assert(a == c);
     assert(*a == 3);
     assert(*c == 3);
